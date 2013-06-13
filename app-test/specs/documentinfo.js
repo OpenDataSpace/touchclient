058988a431266502d933efb3e9524a830eadaf5c
Frederick Roth <f-roth@megaera.de>
2013-06-13 11:26:21 +0200

    DocumentInfo test reflects the status quo
     - checks whether currently displayed fields are loaded

--- app-test/specs/documentinfo.js
+++ app-test/specs/documentinfo.js
@@ -1,9 +1,13 @@
 describe("Documentinfo model", function () {
 
-    it("can be loaded from the server", function () {
+    it("contains all necessary fields", function () {
         var goodSpy = jasmine.createSpy('successSpy'),
             badSpy = jasmine.createSpy('failureSpy'),
-            store = Ext.create("ACMobileClient.store.PrivateGlobalFoldersStore", {});
+            rootstore = Ext.create("ACMobileClient.store.PrivateGlobalFoldersStore"),
+            folderstore = Ext.create("ACMobileClient.store.FolderObjectDataStore"),
+            objectinfostore = Ext.create("ACMobileClient.store.ObjectInfoStore");
+
+
 
         runs(function () {
              ACUtils.utils.login(user, password, goodSpy, badSpy);
@@ -15,15 +19,46 @@ describe("Documentinfo model", function () {
 
         Ext.Ajax.setDefaultHeaders({"Accept":"application/json"});
         runs(function () {
-            store.load();
+            rootstore.load();
         });
 
         waitsFor(function () {
-            return store.isLoaded();
+            return rootstore.isLoaded();
         });
 
         runs(function () {
-            expect(store.getCount()).toBeGreaterThan(0);
+            expect(rootstore.getCount()).toBe(1);
+            folderstore.folderId = rootstore.first().get("id");
+            folderstore.load();
+        });
+
+        waitsFor(function() {
+            return folderstore.isLoaded();
+        });
+
+        runs(function () {
+            expect(folderstore.getCount()).toBe(1);
+            objectinfostore.load({
+                params: {
+                    id: folderstore.first().get("id")
+                }
+            });
+        });
+
+        waitsFor(function() {
+            return objectinfostore.isLoaded();
+        });
+
+        runs(function () {
+           expect(objectinfostore.first().get("name")).toBeDefined(); 
+           expect(objectinfostore.first().get("objecttextkey")).toBeDefined(); 
+           expect(objectinfostore.first().get("description")).toBeDefined(); 
+           expect(objectinfostore.first().get("creator")).toBeDefined(); 
+           expect(objectinfostore.first().get("lastmodifier")).toBeDefined(); 
+           expect(objectinfostore.first().get("owner")).toBeDefined(); 
+           expect(objectinfostore.first().get("createdate")).toBeDefined(); 
+           expect(objectinfostore.first().get("lastmodifydate")).toBeDefined(); 
+           expect(objectinfostore.first().get("updatedate")).toBeDefined(); 
         });
     });
 });
