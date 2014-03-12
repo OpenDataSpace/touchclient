d31e4ac04e54d4d5a3433d7b11ac27a7c886069f
bob-chen <562336543@qq.com>
2014-03-12 14:25:24 +0800

    Fix pull down refresh not work in st 2.3.1
    Change loadStore to fetchLatest.

--- app/utils/PullRefreshFn.js
+++ app/utils/PullRefreshFn.js
@@ -1,28 +1,6 @@
 /**
- * This  pulugin used to extend the Ext.plugin.PulRefresh in Sencha Touch 2.2.0.
+ * This  pulugin used to extend the Ext.plugin.PulRefresh in Sencha Touch 2.3.1.
  * Add refreshFn config option.
- * 
- * ## Example
- *
- *     Ext.create('Ext.dataview.List', {
- *         fullscreen: true,
- *
- *         store: store,
- *
- *         plugins: [
- *             {
- *                 xclass: 'ACMobileClient.utils.PullRefreshFn',
- *                 pullRefreshText: 'Pull down for more new Tweets!'
- *                 refreshFn: function(plugin) { 
- *		                plugin.up().getStore().loadPage(1);
- *		           }
- *             }
- *         ],
- *
- *         itemTpl: [
- *             //itemTpl
- *         ]
- *     });
  */
 Ext.define('ACMobileClient.utils.PullRefreshFn', {
     extend: 'Ext.plugin.PullRefresh',
@@ -39,32 +17,14 @@ Ext.define('ACMobileClient.utils.PullRefreshFn', {
         refreshFn: null
     },
 
-	loadStore: function() {
-	    var me = this,
-	        list = me.getList(),
-	        scroller = list.getScrollable().getScroller();
-	
-	
-	    me.setViewState('loading');
-	    me.isReleased = false;
-	
-	    Ext.defer(function() {
-	        scroller.on({
-	            scrollend: function() {
-	                if (me.getRefreshFn()) {
-	                    me.getRefreshFn().call(me, me);
-	                } else {
-	                    me.fetchLatest();
-	                }
-	                me.resetRefreshState();
-	            },
-	            delay: 100,
-	            single: true,
-	            scope: me
-	        });
-	        scroller.minPosition.y = 0;
-	        scroller.scrollTo(null, 0, true);
-	    }, 500, me);
-	
-	}
+    fetchLatest: function(){
+    	if(this.getRefreshFn()){
+    		this.getRefreshFn().call(this, this);
+    		this.setState("loaded");
+    		this.fireEvent("latestfetched", this, 'refreshFn');
+    		if(this.getAutoSnapBack()){
+    			this.snapBack();
+    		}
+    	}
+    }
 });
